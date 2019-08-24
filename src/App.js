5bca02ba00f0431adf7f21a1405cf0c53c2efd30
popmac <parade8823@gmail.com>
2019-08-24 17:31:57 +0900

    MemoListの切り出し

--- src/App.js
+++ src/App.js
@@ -5,10 +5,35 @@ import './App.css';
 import axios from './axiosSetting.js';
 import update from 'react-addons-update';
 import { Redirect, Route, Switch } from 'react-router';
-import { Link } from 'react-router-dom';
+// @material-ui の Link と衝突するので RouterLink にしている
+import { Link as RouterLink } from 'react-router-dom';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import SignInSide from './components/SignIn';
 import ShareInput from "./components/ShareInput";
+import MemoList from "./components/MemoList";
 
 class App extends Component {
   constructor() {
@@ -66,9 +91,16 @@ class App extends Component {
       <div className="App">
         <header>
           <h1>
-            <Link to='/'>Memotter</Link>
+            <RouterLink to='/'>Memotter</RouterLink>
           </h1>
         </header>
+
+        <Grid container component="main" className="left-container">
+          <CssBaseline/>
+
+          <MemoList memos={this.state.memos} />
+        </Grid>
+
         <SignInSide memos={this.state.memos}/>
         <ShareInput value={this.state.inputValue} changeInputValue={this.changeInputValue} addPost={this.addPost}/>
 
