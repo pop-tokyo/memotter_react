97259a976ea037207cf65bbbc0b0bd9fd5de3276
SayaHoribe <s.horibe0831@gmail.com>
2019-09-15 19:14:05 +0900

    success refirect to world

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,8 +2,8 @@ import React from "react";
 import {Redirect} from "react-router";
 
 export default function Auth(props) {
-  const token = localStorage.getItem('accessToken');
+  const access_token = localStorage.getItem('accessToken');
   return (
-     token != null ? props.children : <Redirect to={'/sign_up'}/>
+    access_token != null ? props.children : <Redirect to={'/sign_up'}/>
 )
 }
