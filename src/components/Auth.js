1c0fc5b82ec44c36e492d302f1a9d4f949ee4ad9
SayaHoribe <s.horibe0831@gmail.com>
2019-09-20 12:25:32 +0900

    delete

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,13 +1,9 @@
 import React from "react";
 import {Redirect} from "react-router";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import List from "@material-ui/core/List";
 
 export default function Auth(props) {
   const access_token = localStorage.getItem('accessToken');
   const current_path = props.location.pathname;
-  /* eslint no-unused-expressions: "off" */
   return(
     (() => {
       if (access_token != null) {
